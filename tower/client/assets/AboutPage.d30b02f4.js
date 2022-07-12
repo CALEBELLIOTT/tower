12789ea73d23cf23c7f16d55b47e2e04d35c9d44
Caleb Elliott <caleb123elliott@gmail.com>
2022-07-12 09:46:44 -0600

    builds

---
+++ tower/client/assets/AboutPage.d30b02f4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.fcd37509.js";import{o,b as t,d as s}from"./vendor.fc65a91a.js";const a={name:"AboutPage"},c={class:"about"},n=s("h1",null,"This is the about page",-1),r=[n];function _(d,i,p,f,u,h){return o(),t("div",c,r)}var b=e(a,[["render",_]]);export{b as default};
